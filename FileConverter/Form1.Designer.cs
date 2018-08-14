f999480e572bf4d8fd7e30c86d248219e0513532
Evan <emurray728@gmail.com>
2018-08-13 22:01:34 -0400

    files can successfully convert into hardcoded file types

--- FileConverter/Form1.Designer.cs
+++ FileConverter/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.inputFileBox = new System.Windows.Forms.TextBox();
             this.browseButton = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.outputButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -56,22 +58,41 @@
             // 
             this.browseButton.Location = new System.Drawing.Point(553, 73);
             this.browseButton.Name = "browseButton";
-            this.browseButton.Size = new System.Drawing.Size(102, 23);
+            this.browseButton.Size = new System.Drawing.Size(102, 33);
             this.browseButton.TabIndex = 2;
             this.browseButton.Text = "Browse...";
             this.browseButton.UseVisualStyleBackColor = true;
             this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(246, 195);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(290, 22);
+            this.outputTextBox.TabIndex = 3;
+            // 
+            // outputButton
+            // 
+            this.outputButton.Location = new System.Drawing.Point(553, 195);
+            this.outputButton.Name = "outputButton";
+            this.outputButton.Size = new System.Drawing.Size(102, 32);
+            this.outputButton.TabIndex = 4;
+            this.outputButton.Text = "Output";
+            this.outputButton.UseVisualStyleBackColor = true;
+            this.outputButton.Click += new System.EventHandler(this.outputButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputButton);
+            this.Controls.Add(this.outputTextBox);
             this.Controls.Add(this.browseButton);
             this.Controls.Add(this.inputFileBox);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Image Converter";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,6 +103,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox inputFileBox;
         private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Button outputButton;
     }
 }
 
--- FileConverter/Form1.cs
+++ FileConverter/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,7 +20,15 @@ namespace FileConverter
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            string fileName = inputFileBox.Text;
+            Image image = Image.FromFile(fileName);
+            DirectoryInfo di = new DirectoryInfo(fileName);
+            
+            string outputFile = outputTextBox.Text + "\\" + di.Name.Substring(0, di.Name.Length - 3)+"png";
+            image.Save(outputFile, System.Drawing.Imaging.ImageFormat.Png);
+            
+            
+            
         }
 
         private void browseButton_Click(object sender, EventArgs e)
@@ -39,6 +48,16 @@ namespace FileConverter
         private void inputFileBox_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void outputButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.ShowNewFolderButton = true;
+            if(folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                outputTextBox.Text = folderBrowserDialog.SelectedPath;
+            }
         }
     }
 }

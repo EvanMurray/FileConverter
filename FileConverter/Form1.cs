2c2591f21cedb2964b54c2d2ca1f8d1910806d3e
Evan <emurray728@gmail.com>
2018-08-16 09:49:05 -0400

    added in-line comment documentation

--- FileConverter/Form1.cs
+++ FileConverter/Form1.cs
@@ -15,6 +15,7 @@ namespace FileConverter
 {
     public partial class Form1 : Form
     {
+        //initializes an empty image object for storing the users image 
         Image image;
         public Form1()
         {
@@ -22,18 +23,23 @@ namespace FileConverter
             formatMenu.SelectedIndex = 0;
         }
 
-       
+        //button used to convert your selected image 
         private void convertButton_Click(object sender, EventArgs e)
         {
+            //checks to ensure textboxes are not left blank
             if (!(String.IsNullOrEmpty(inputFileBox.Text)) && !(String.IsNullOrEmpty(outputTextBox.Text)))
             {
+                //obtains the file name and uses DirectoryInfo to give access to just the file name and not the entire path
                 string fileName = inputFileBox.Text;
-
                 DirectoryInfo di = new DirectoryInfo(fileName);
+
                 string fileType = comboBoxChoice().ToLower();
 
-                string outputFile = outputTextBox.Text + "\\" + di.Name.Substring(0, di.Name.Length - 3) + fileType;
+                //Saves the image based on the combo box choice using the Image class
+                string outputFile = outputTextBox.Text + @"\" + di.Name.Substring(0, di.Name.Length - 3) + fileType;
                 image.Save(outputFile, getImageFormat(fileType));
+
+                //displays a success message and prompts the user if they wish to continue
                 DialogResult result = MessageBox.Show("Success! Did you want to convert another image?", "Conversion Successful", MessageBoxButtons.YesNo);
                 resultChoice(result);
                 
@@ -44,9 +50,10 @@ namespace FileConverter
             }
         }
 
-
+        //button to choose the image the user wishes to convert
         private void browseButton_Click(object sender, EventArgs e)
         {
+            //allows the user to choose their file using the File Dialog
             OpenFileDialog fdlg = new OpenFileDialog();
             fdlg.Title = "Please choose a file";
             fdlg.InitialDirectory = @"c:\";
@@ -57,6 +64,7 @@ namespace FileConverter
             {
                 inputFileBox.Text = fdlg.FileName;
 
+                //tries to save in your file as an image, but catches if the file type is incompatible with the Image class
                 try
                 {
                     image = Image.FromFile(inputFileBox.Text);
@@ -68,14 +76,15 @@ namespace FileConverter
                     inputFileBox.Clear();
                     
                 }
-                
+                //sets the image to the picture box
                 displayBox.Image = image;
             }
         }
 
-   
+        //button to select the folder to where the newly converted file will be saved
         private void outputButton_Click(object sender, EventArgs e)
         {
+            
             FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
             folderBrowserDialog.ShowNewFolderButton = true;
             if(folderBrowserDialog.ShowDialog() == DialogResult.OK)
@@ -84,13 +93,14 @@ namespace FileConverter
             }
         }
 
-
+        //Stores the value of the users choice in the combo box used for file type selection
         private string comboBoxChoice()
         {
             string type = formatMenu.Text;
             return type;
         }
 
+        //gets the image format that the user picked to correctly save the new file
         private ImageFormat getImageFormat(string type)
         {
             switch (type)
@@ -109,6 +119,7 @@ namespace FileConverter
             
         }
 
+        //function for the choice made in the success messagebox 
         private void resultChoice(DialogResult result)
         {
             if (result == DialogResult.Yes)
